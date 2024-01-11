d6c9e6613d00fd8552b4fd5247c1b9ac952fc0a5
Carlos Florêncio <carlosmflorencio@gmail.com>
2024-01-11 21:51:36 +0000

    add sidebar functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,26 @@
 import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
-  vscode.window.showInformationMessage("activated")
-  // listen for changes in the visible text editors
-  vscode.window.onDidChangeVisibleTextEditors(async (editors) => {
-    await manageWindows(editors)
-  })
+  context.subscriptions.push(
+    vscode.window.onDidChangeVisibleTextEditors((editors) => {
+      autoCloseSidebar(editors)
+    })
+  )
 }
-
-// This method is called when your extension is deactivated
-// export function deactivate() {}
 
 let previousEditorsCount = 0
 
 // close the sidebar if there are two editors open side by side
-async function manageWindows(editors: Readonly<vscode.TextEditor[]>) {
-  const visibleEditors = vscode.window.visibleTextEditors
-  const sidebarVisible = isSidebarVisible()
-
-  vscode.window.showInformationMessage(
-    `Visible editors: ${visibleEditors.length}, received editors: ${editors.length} sidebar visible: ${sidebarVisible}`
-  )
-
-  console.log(
-    "context",
-    await vscode.commands.executeCommand("getContextKeyInfo")
-  )
-  // If there are two editors open side by side
-  if (visibleEditors.length === 2) {
-    if (isSidebarVisible()) {
-      // Close the sidebar
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleSidebarVisibility"
-      )
-    }
+function autoCloseSidebar(editors: Readonly<vscode.TextEditor[]>) {
+  // opening side by side
+  if (editors.length === 2 && previousEditorsCount === 1) {
+    vscode.commands.executeCommand("workbench.action.closeSidebar")
   }
-}
 
-function isSidebarVisible() {
-  console.log("here workbench", vscode.workspace.getConfiguration("workbench"))
-  return vscode.workspace
-    .getConfiguration("workbench")
-    .get("sidebar.visible") as boolean
+  // back to a single editor
+  if (editors.length === 1 && previousEditorsCount === 2) {
+    vscode.commands.executeCommand("workbench.action.toggleSidebarVisibility")
+  }
+
+  previousEditorsCount = editors.length
 }
