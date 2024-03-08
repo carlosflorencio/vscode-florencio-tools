a988c92a511ad866abef16b856c6f592e51468f5
Carlos Florêncio <carlosmflorencio@gmail.com>
2024-03-08 15:01:16 +0000

    add missing subscription

--- src/auto-editor-width.ts
+++ src/auto-editor-width.ts
@@ -12,42 +12,44 @@ export function registerAutoEditorWidth(context: ExtensionContext) {
   let lastActiveGroup = window.tabGroups.activeTabGroup
   let didChangeSize = false
 
-  window.onDidChangeActiveTextEditor(async (editor) => {
-    if (!editor) {
-      lastActiveGroup = window.tabGroups.activeTabGroup
-      return
-    }
-
-    // wait for the editor to be fully active
-    setTimeout(async () => {
-      const splitCount = window.tabGroups.all.length
-
-      if (splitCount < 3) {
-        // restore balance after all the resizes done
-        if (didChangeSize) {
-          await commands.executeCommand("workbench.action.evenEditorWidths")
-          didChangeSize = false
-        }
-
+  context.subscriptions.push(
+    window.onDidChangeActiveTextEditor(async (editor) => {
+      if (!editor) {
         lastActiveGroup = window.tabGroups.activeTabGroup
         return
       }
 
-      // moved to another editor group
-      if (lastActiveGroup !== window.tabGroups.activeTabGroup) {
-        // balance the editor widths
-        await commands.executeCommand("workbench.action.evenEditorWidths")
+      // wait for the editor to be fully active
+      setTimeout(async () => {
+        const splitCount = window.tabGroups.all.length
 
-        // increase multiple times to make the active editor bigger
-        for (let i = 0; i < splitCount + 2; i++) {
-          await commands.executeCommand("workbench.action.increaseViewWidth")
+        if (splitCount < 3) {
+          // restore balance after all the resizes done
+          if (didChangeSize) {
+            await commands.executeCommand("workbench.action.evenEditorWidths")
+            didChangeSize = false
+          }
+
+          lastActiveGroup = window.tabGroups.activeTabGroup
+          return
         }
 
-        // mark as changed to restore balance later
-        didChangeSize = true
-      }
+        // moved to another editor group
+        if (lastActiveGroup !== window.tabGroups.activeTabGroup) {
+          // balance the editor widths
+          await commands.executeCommand("workbench.action.evenEditorWidths")
 
-      lastActiveGroup = window.tabGroups.activeTabGroup
-    }, 20)
-  })
+          // increase multiple times to make the active editor bigger
+          for (let i = 0; i < splitCount + 2; i++) {
+            await commands.executeCommand("workbench.action.increaseViewWidth")
+          }
+
+          // mark as changed to restore balance later
+          didChangeSize = true
+        }
+
+        lastActiveGroup = window.tabGroups.activeTabGroup
+      }, 20)
+    })
+  )
 }
