d3e3e605863b84a461321e4ef7231c5bfeafce5c
Carlos Florêncio <carlosmflorencio@gmail.com>
2025-01-02 20:41:21 +0000

    improve auto editor width

--- src/auto-editor-width.ts
+++ src/auto-editor-width.ts
@@ -1,6 +1,8 @@
 import { ExtensionContext, commands, window } from "vscode"
 import config from "./config"
 
+let editorGroupSizesEnabled = false
+
 /**
  * Increments the active editor width when switching between editors groups
  */
@@ -9,47 +11,35 @@ export function registerAutoEditorWidth(context: ExtensionContext) {
     return
   }
 
-  let lastActiveGroup = window.tabGroups.activeTabGroup
-  let didChangeSize = false
+  const checkAndToggleEditorWidths = async () => {
+    const splitCount = window.tabGroups.all.length
+
+    if (splitCount < 3) {
+      if (editorGroupSizesEnabled) {
+        editorGroupSizesEnabled = false
+        await commands.executeCommand("workbench.action.evenEditorWidths")
+      }
+      return
+    }
+
+    if (!editorGroupSizesEnabled) {
+      editorGroupSizesEnabled = true
+      await commands.executeCommand("workbench.action.minimizeOtherEditors")
+    }
+  }
 
   context.subscriptions.push(
     window.onDidChangeActiveTextEditor(async (editor) => {
       if (!editor) {
-        lastActiveGroup = window.tabGroups.activeTabGroup
+        await window.showInformationMessage("No active editor")
         return
       }
 
       // wait for the editor to be fully active
-      setTimeout(async () => {
-        const splitCount = window.tabGroups.all.length
-
-        if (splitCount < 3) {
-          // restore balance after all the resizes done
-          if (didChangeSize) {
-            await commands.executeCommand("workbench.action.evenEditorWidths")
-            didChangeSize = false
-          }
-
-          lastActiveGroup = window.tabGroups.activeTabGroup
-          return
-        }
-
-        // moved to another editor group
-        if (lastActiveGroup !== window.tabGroups.activeTabGroup) {
-          // balance the editor widths
-          await commands.executeCommand("workbench.action.evenEditorWidths")
-
-          // increase multiple times to make the active editor bigger
-          for (let i = 0; i < splitCount + 2; i++) {
-            await commands.executeCommand("workbench.action.increaseViewWidth")
-          }
-
-          // mark as changed to restore balance later
-          didChangeSize = true
-        }
-
-        lastActiveGroup = window.tabGroups.activeTabGroup
-      }, 20)
+      setTimeout(checkAndToggleEditorWidths, 20)
     })
   )
+
+  // Check on startup
+  setTimeout(checkAndToggleEditorWidths, 20)
 }
